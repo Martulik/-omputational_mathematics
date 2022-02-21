0c152204fe49c5e1dc776c9e736d2cfaa9115ebe
Marta <ezerinya.m@edu.spbstu.ru>
2022-02-21 12:22:50 +0300

    fix second matrix

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -3,25 +3,6 @@
 #include <FORSYTHE.H>
 #include "data.hpp"
 
-std::vector< std::vector< double>> inverse_matrix(DECOMP *const decomp)
-{
-  std::vector< double > e(size_of_matrix, 0);
-  std::vector< double > b(size_of_matrix);
-  std::vector< std::vector< double>> inv_a(size_of_matrix, std::vector< double >(size_of_matrix));
-  for (int i = 0; i < size_of_matrix; ++i) {
-    e.at(i) = 1;
-    if (i != 0) {
-      e.at(i - 1) = 0;
-    }
-    b = e;
-    decomp->Solve(b);
-    for (int j = 0; j < size_of_matrix; ++j) {
-      inv_a[j][i] = b[j];
-    }
-  }
-  return inv_a;
-}
-
 std::vector< std::vector< double>> matrix_multiplication(const std::vector< std::vector< double>> &a,
                                                          const std::vector< std::vector< double>> &b)
 {
@@ -39,22 +20,9 @@ std::vector< std::vector< double>> matrix_multiplication(const std::vector< std:
 
 void output(const double p_i, const double cond_a, const double cond_at, const double delta)
 {
-  std::cout << std::setw(20) << "P" << std::setw(30) << "COND(A)" << std::setw(30) << "COND(A^t*A)" << "delta" << '\n';
-  std::cout << std::setw(20) << p_i << std::setw(30) << cond_a << std::setw(30) << cond_at << delta << '\n';
-  std::cout << "--------------------------------------------------\n";
-}
-
-double matrix_norm(const std::vector< std::vector< double>> &matrix) // по строкам
-{
-  double max = 0.0;
-  for (int i = 0; i < size_of_matrix; ++i) {
-    double sum = 0.0;
-    for (int j = 0; j < size_of_matrix; ++j) {
-      sum += abs(matrix[i][j]);
-    }
-    max = max > sum ? max : sum;
-  }
-  return max;
+  std::cout << std::setw(20) << "P" << std::setw(30) << "COND(A)" << std::setw(35) << "COND(A^t*A)" << "delta" << '\n';
+  std::cout << std::setw(20) << p_i << std::setw(30) << cond_a << std::setw(35) << cond_at << delta << '\n';
+  std::cout << '\n';
 }
 
 std::vector< std::vector< double>> transpose_matrix(const std::vector< std::vector< double>> &a)
@@ -92,14 +60,11 @@ void solve_systems()
     std::vector< double > b1 = B(p[i]);
     DECOMP decomp(size_of_matrix, a);
     decomp.Solve(b1);
+    double cond1 = decomp.Cond();
 
-    std::vector< std::vector< double>> inv_a = inverse_matrix(&decomp);
-    double cond1 = matrix_norm(a) * matrix_norm(inv_a);
-
-    std::vector< double > b2 = B(p[0]);
     std::vector< std::vector< double>> trans_a = transpose_matrix(a);
-    std::vector< std::vector< double>> trans_a_a = matrix_multiplication(trans_a, a);
-    DECOMP decomp2(size_of_matrix, trans_a_a);
+    std::vector< double > b2 = B(p[i]);
+    DECOMP decomp2(size_of_matrix, matrix_multiplication(trans_a, a));
 
     std::vector< double > trans_a_b2(b2.size(), 0);
     for (int i = 0; i < trans_a_b2.size(); ++i) {
@@ -110,7 +75,7 @@ void solve_systems()
     decomp2.Solve(trans_a_b2);
 
     double delta = calculate_delta(b1, trans_a_b2);
-    double cond2 = matrix_norm(trans_a_a) * matrix_norm(inverse_matrix(&decomp2));
+    double cond2 = decomp2.Cond();
 
     output(p[i], cond1, cond2, delta);
   }
@@ -124,3 +89,5 @@ int main()
 
   return 0;
 }
+
+// зачем умножать на транспон? для симметрической матрицы. 
