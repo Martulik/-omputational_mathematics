b3238899f870b533cf847f8ef6b11bd164fa99a5
Marta <ezerinya.m@edu.spbstu.ru>
2022-03-03 23:04:31 +0300

    fix f_k

--- estimation_parameters/main.cpp
+++ estimation_parameters/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <FORSYTHE.H>
+#include <iomanip>
 #include "data.hpp"
 
 int main()
 {
-  double k = FMIN(36, 46, f_k, eps);
-  std::cout << "k = " << k << '\n';
+  double k = FMIN(36, 46, f_k, 0.0001);
+  std::cout << "k = " << std::fixed << std::setprecision(10) << k << '\n';
+  std::cout << "f(k + 0.2) = " << std::fixed << std::setprecision(10) << f_k(k + 0.2) << '\n';
+  std::cout << "f(k + 0.1) = " << std::fixed << std::setprecision(10) << f_k(k + 0.1) << '\n';
+  std::cout << "f(k) = " << std::fixed << std::setprecision(10) << f_k(k) << '\n';
+  std::cout << "f(k - 0.1) = " << std::fixed << std::setprecision(10) << f_k(k - 0.1) << '\n';
+  std::cout << "f(k - 0.2) = " << std::fixed << std::setprecision(10) << f_k(k - 0.2) << '\n';
 
   return 0;
 }
