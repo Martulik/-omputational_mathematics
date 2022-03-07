003bc2b05c53378673910cbaf38d807463216fd9
Marta <ezerinya.m@edu.spbstu.ru>
2022-03-07 12:45:32 +0300

    final working case

--- estimation_parameters/data.hpp
+++ estimation_parameters/data.hpp
@@ -1,7 +1,6 @@
 #ifndef DATA_HPP
 #define DATA_HPP
 
-#include <cmath>
 #include "FORSYTHE.H"
 
 double eps = 0.00001;
@@ -31,61 +30,40 @@ double l()
 
 void f(const double k, const double *y, double *yp)
 {
-//  yp[0] = y[1];
-//  yp[1] = -((k_global / M) * y[0] + g * (1 - std::cos(y[2])) - (1 + y[0]) * y[3] * y[3]);
-//  yp[2] = y[3];
-//  yp[3] = -(g / (l() + y[0]) * std::sin(y[2]) + 2 / (1 + y[0]) + y[1] * y[3]);
- // std::cout << yp[0] << ' ' << yp[1] << ' ' << yp[2] << ' ' << yp[3] << '\n';
- yp[0] = (y[2] * y[2] - k_global / M) * y[1] + l() * y[2] * y[2] - g * (1 - std::cos(y[3]));
-yp[1] = y[0];
-yp[2] = (-2 * y[0] / (l() + y[1])) * y[2] - (g * std::sin(y[3]) / (l() + y[1]));
-yp[3] = y[2];
+  yp[0] = (y[2] * y[2] - k_global / M) * y[1] + l() * y[2] * y[2] - g * (1 - std::cos(y[3]));
+  yp[1] = y[0];
+  yp[2] = (-2 * y[0] / (l() + y[1])) * y[2] - (g * std::sin(y[3]) / (l() + y[1]));
+  yp[3] = y[2];
 }
-
-
 
 void (*f_ptr)(double, double *, double *) = reinterpret_cast<void (*)(double, double *, double *)>(f);
 
-//void x_mod_fill()
-//{
-//  double tout = 0.4;
-//  int neqn = 4;
-//  double work[6 * 4 + 3] = {0.0};
-//  x_mod[0] = 0.0;
-//  int iflag = 1;
-//  for (int i = 1; i < N; ++i) {
-//    double t = 0.0;
-//    double x_mod_p[4] = {0.0, 0.0, 0.0, 4.0};
-//    RKF45(f_ptr, neqn, x_mod_p, t, tout, eps, eps, work, iflag);
-//    x_mod[i] = x_mod_p[0];
-//    tout += 0.4;
-//  }
-//}
+void x_mod_fill()
+{
+  double tout = 0.0;
+  int neqn = 4;
+  double work[6 * 4 + 3] = {0.0};
+  for (int i = 1; i < N; ++i) {
+    double t = 0.0;
+    int iflag = 1;
+    double x_mod_p[4] = {0.0, 0.0, 4.0, 0.0};
+    tout += 0.4;
+    while (abs(t - tout) > eps) {
+      RKF45(f_ptr, neqn, x_mod_p, t, tout, eps, eps, work, iflag);
+    }
+    x_mod[i] = x_mod_p[1];
+  }
+}
 
 double f_k(double k)
 {
   k_global = k;
   //std::cout << "F(K): k = " << k << '\n';
   double sum = 0.0;
-  double tout = 0.0;
-  int neqn = 4;
-  double work[6 * 4 + 3] = {0.0};
-  int iflag = 1; // k = 36
-  double t = 0.0;
+  x_mod_fill();
   for (int i = 1; i < N; ++i) {
-    tout += 0.4;
-    iflag = 1;
-    t = 0.0;
-    //double x_mod_p[4] = {0.0, 0.0, 0.0, 4.0};
-    double x_mod_p[4] = {0.0, 0.0, 4.0, 0.0};
-
-    while (abs(t - tout) > eps) {
-      RKF45(f_ptr, neqn, x_mod_p, t, tout, eps, eps, work, iflag);
-      //std::cout << "tout = " << tout << "\t t = " << t << "\t iflag = " << iflag << '\n';
-    }
-    sum += std::pow(x_exp[i] - x_mod_p[1], 2);
+    sum += std::pow(x_exp[i] - x_mod[i], 2);
   }
-  //std::cout << "tout = " << tout << "\t t = " << t << "\t iflag = " << iflag << '\n';
   return sum;
 }
 
--- estimation_parameters/main.cpp
+++ estimation_parameters/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <FORSYTHE.H>
 #include <iomanip>
 #include "data.hpp"
 
 int main()
 {
   double k = FMIN(36, 46, f_k, 0.0001);
+
   std::cout << "k = " << std::fixed << std::setprecision(10) << k << '\n';
   std::cout << "f(k + 0.2) = " << std::fixed << std::setprecision(10) << f_k(k + 0.2) << '\n';
   std::cout << "f(k + 0.1) = " << std::fixed << std::setprecision(10) << f_k(k + 0.1) << '\n';
